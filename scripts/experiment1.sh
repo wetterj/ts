b15fbd4096fa58a094653776aec214ad20e61026
James Wetter <wetter.j@gmail.com>
2016-10-15 14:17:25 +0100

    first exp script

---
+++ scripts/experiment1.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# this experiment should demonstrate the power of the proagators
+# run the coplete search with inorder branching on some small instances
+
+SEED=0
+TIMEOUT=120000
+THREADS=4
+
+# The directory to put the tmp stuff
+dir=$(mktemp -d 'exp1-XXXXX')
+instanceDir=${dir}/instances
+mkdir ${instanceDir}
+instances=''
+
+# generate some instances
+TELE=1
+TARG=10
+HORI=10
+MIN_GAIN=1
+MAX_GAIN=5
+MIN_CAD=5
+MAX_CAD=5
+MIN_P=1
+MAX_P=1
+VIS_P=0
+OFFSET=0
+BALANCE_N=1
+BALANCE_D=2
+
+OUTPUT=${instanceDir}/tele${TELE}.targ${TARG}.hori${HORI}.minGain${MIN_GAIN}.maxGain${MAX_GAIN}.minCadence${MIN_CAD}.maxCadence${MAX_CAD}.minPeriod${MIN_P}.maxPeriod${MAX_P}.vis${VIS_P}.offset${OFFSET}.balanceN${BALANCE_N}.balanceD${BALANCE_D}
+python telescopes/python/mkInstance.py $SEED $TELE $TARG $HORI $MIN_GAIN $MAX_GAIN $MIN_CAD $MAX_CAD $MIN_P $MAX_P $VIS_P $OFFSET $BALANCE_N $BALANCE_D $OUTPUT
+instances=${instances}' '${OUTPUT}
+
+TELE=2
+TARG=10
+HORI=10
+MIN_GAIN=1
+MAX_GAIN=5
+MIN_CAD=5
+MAX_CAD=5
+MIN_P=1
+MAX_P=1
+VIS_P=0
+OFFSET=0
+BALANCE_N=1
+BALANCE_D=2
+
+OUTPUT=${instanceDir}/tele${TELE}.targ${TARG}.hori${HORI}.minGain${MIN_GAIN}.maxGain${MAX_GAIN}.minCadence${MIN_CAD}.maxCadence${MAX_CAD}.minPeriod${MIN_P}.maxPeriod${MAX_P}.vis${VIS_P}.offset${OFFSET}.balanceN${BALANCE_N}.balanceD${BALANCE_D}
+python telescopes/python/mkInstance.py $SEED $TELE $TARG $HORI $MIN_GAIN $MAX_GAIN $MIN_CAD $MAX_CAD $MIN_P $MAX_P $VIS_P $OFFSET $BALANCE_N $BALANCE_D $OUTPUT
+instances=${instances}' '${OUTPUT}
+
+TELE=2
+TARG=15
+HORI=100
+MIN_GAIN=1
+MAX_GAIN=5
+MIN_CAD=5
+MAX_CAD=5
+MIN_P=9
+MAX_P=11
+VIS_P=0
+OFFSET=0
+BALANCE_N=3
+BALANCE_D=5
+
+OUTPUT=${instanceDir}/tele${TELE}.targ${TARG}.hori${HORI}.minGain${MIN_GAIN}.maxGain${MAX_GAIN}.minCadence${MIN_CAD}.maxCadence${MAX_CAD}.minPeriod${MIN_P}.maxPeriod${MAX_P}.vis${VIS_P}.offset${OFFSET}.balanceN${BALANCE_N}.balanceD${BALANCE_D}
+python telescopes/python/mkInstance.py $SEED $TELE $TARG $HORI $MIN_GAIN $MAX_GAIN $MIN_CAD $MAX_CAD $MIN_P $MAX_P $VIS_P $OFFSET $BALANCE_N $BALANCE_D $OUTPUT
+instances=${instances}' '${OUTPUT}
+
+TELE=3
+TARG=15
+HORI=100
+MIN_GAIN=1
+MAX_GAIN=5
+MIN_CAD=5
+MAX_CAD=5
+MIN_P=9
+MAX_P=11
+VIS_P=0
+OFFSET=0
+BALANCE_N=3
+BALANCE_D=5
+
+OUTPUT=${instanceDir}/tele${TELE}.targ${TARG}.hori${HORI}.minGain${MIN_GAIN}.maxGain${MAX_GAIN}.minCadence${MIN_CAD}.maxCadence${MAX_CAD}.minPeriod${MIN_P}.maxPeriod${MAX_P}.vis${VIS_P}.offset${OFFSET}.balanceN${BALANCE_N}.balanceD${BALANCE_D}
+python telescopes/python/mkInstance.py $SEED $TELE $TARG $HORI $MIN_GAIN $MAX_GAIN $MIN_CAD $MAX_CAD $MIN_P $MAX_P $VIS_P $OFFSET $BALANCE_N $BALANCE_D $OUTPUT
+instances=${instances}' '${OUTPUT}
+
+# Run all the configurations on all instances
+parallel -j ${THREADS} echo ./bin/completeSearch ::: ${instances} ::: ${TIMEOUT} ::: o ::: f t ::: f t ::: f t
+
+rm -rf $dir
