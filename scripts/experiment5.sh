4db1b0a578c7e25bfa950f32ec3a449a4ff4fe57
James Wetter <wetter.j@gmail.com>
2016-11-10 14:07:31 +0000

    updated SA

--- scripts/experiment5.sh
+++ scripts/experiment5.sh
@@ -8,15 +8,15 @@ THREADS=4
 INIT_TOS='1000'
 NH_TOS='500'
 PHIS='100'
-INIT_TEMP='0.5 0.75 1.0 1.25 1.5'
-COOL_RATE='0.001 0.0031622776602 0.01 0.031622776602 0.1 0.31622776602'
+N_ANTS='4 8 16 32'
+RHO='0.1 0.2 0.3'
 SEEDS=$(seq 5)
 
 # The directory to put the tmp stuff
-dir=$(mktemp -d 'exp4-XXXXX')
+dir=$(mktemp -d 'exp5-XXXXX')
 instanceDir=${dir}/instances
 mkdir ${instanceDir}
-outdir=experiments/exp4/
+outdir=experiments/exp5/
 mkdir -p ${outdir}
 
 # generate some instances
@@ -27,25 +27,25 @@ instances="${instances} ${instanceDir}/tele5.targ200.hori1000.minGain10.maxGain1
 instances="${instances} ${instanceDir}/tele2.targ200.hori500.minGain10.maxGain100.minCadence20.maxCadence100.minPeriod8.maxPeriod12.vis10.offset50.0.balanceN3.balanceD5"
 
 # Run all the configurations on all instances
-function runSA {
+function runACO {
   outd=$1
   inst=$2
   to=$3
   init_to=$4
   nh_to=$5
   phi=$6
-  init_tmp=$7
-  cool_rate=$8
+  n_ants=$7
+  rho=$8
   seed=$9
-  outFile=${outd}/${nh_to}-${phi}-${init_tmp}-${cool_rate}-${seed}-$(basename ${inst})
+  outFile=${outd}/${nh_to}-${phi}-${n_ants}-${rho}-${seed}-$(basename ${inst})
   if [ ! -f ${outFile} ]; then
-    ./bin/simulatedAnnealing ${seed} ${inst} ${to} ${init_to} ${nh_to} ${phi} ${init_tmp} ${cool_rate} ${outFile}
+    ./bin/aco ${seed} ${inst} ${to} ${init_to} ${nh_to} ${phi} ${n_ants} ${rho} ${outFile}
   fi
 }
 
-export -f runSA
+export -f runACO
 
-parallel -j ${THREADS} runSA ${outdir} ::: ${instances} ::: ${TIMEOUT} ::: ${INIT_TOS} ::: ${NH_TOS} ::: ${PHIS} ::: ${INIT_TEMP} ::: ${COOL_RATE} ::: ${SEEDS}
+parallel -j ${THREADS} runACO ${outdir} ::: ${instances} ::: ${TIMEOUT} ::: ${INIT_TOS} ::: ${NH_TOS} ::: ${PHIS} ::: ${N_ANTS} ::: ${RHO} ::: ${SEEDS}
 
 rm -rf $dir
 
